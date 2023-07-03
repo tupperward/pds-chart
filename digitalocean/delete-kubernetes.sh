ae66445d180baedba466c1c8bee8a0ada8d723fb
Ben <ben@bensapp.com>
2023-07-03 16:26:26 -0400

    initial scripts and README addition for digital ocean

---
+++ digitalocean/delete-kubernetes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+while getopts c:k: args
+do
+    case "${args}" in
+        c) context=${OPTARG};;
+        k) cluster=${OPTARG};;
+    esac
+done
+
+doctl --context "$context" kubernetes cluster delete "$cluster"
