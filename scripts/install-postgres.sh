087d7572103994dc9722c6591c3ae101b336717d
Ben <ben@bensapp.com>
2023-07-03 10:24:56 -0400

    remove tab from script

--- scripts/install-postgres.sh
+++ scripts/install-postgres.sh
@@ -3,6 +3,6 @@
 set -e
 
 helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql \
-	--set global.postgresql.auth.username=pds \
+  --set global.postgresql.auth.username=pds \
   --set global.postgresql.auth.password=pds \
   --set global.postgresql.auth.database=pds
