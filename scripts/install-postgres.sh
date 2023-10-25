ec32c18e0363454209c36be3db618c1060160547
Alex Ward <alex.ward@immuta.com>
2023-10-25 04:11:20 -0400

    create postgres as dependency, delete script

--- scripts/install-postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql \
-  --set global.postgresql.auth.username=pds \
-  --set global.postgresql.auth.password=pds \
-  --set global.postgresql.auth.database=pds
